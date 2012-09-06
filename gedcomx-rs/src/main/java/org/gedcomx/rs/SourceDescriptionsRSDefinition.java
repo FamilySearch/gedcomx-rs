7018ea9911b72520d3c85fcd3d6d70adc4d4167c
thomast73 <thad.thomas@familysearch.org>
2012-09-06 17:47:19 -0600

    Refactor of the refactor of the Source Box API.

--- gedcomx-rs/src/main/java/org/gedcomx/rs/SourceDescriptionsRSDefinition.java
+++ gedcomx-rs/src/main/java/org/gedcomx/rs/SourceDescriptionsRSDefinition.java
@@ -22,7 +22,6 @@ import org.gedcomx.rt.rs.ResourceDefinition;
 import org.gedcomx.rt.rs.ResponseCode;
 import org.gedcomx.rt.rs.StatusCodes;
 
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.core.Response;
 
@@ -42,20 +41,7 @@ public interface SourceDescriptionsRSDefinition extends CommonRSParameters {
   public static final String REL = GEDCOMX_LINK_REL_PREFIX + "source/descriptions";
 
   /**
-   * Get all of this user's source descriptions.
-   *
-   * @return The user's Source Box (Folders).
-   */
-  @GET
-  @StatusCodes ( {
-                   @ResponseCode ( code = 200, condition = "Upon a successful read." ),
-                   @ResponseCode ( code = 400, condition = "If the request was unable to be understood by the application." ),
-                   @ResponseCode ( code = 404, condition = "If the requested page or folder is not found." )
-                 } )
-  Response get();
-
-  /**
-   * Add a source description to the user's source descriptions.
+   * Add a source description.
    *
    * @param sourceDescription The source description to be added.
    * @return The appropriate response.
