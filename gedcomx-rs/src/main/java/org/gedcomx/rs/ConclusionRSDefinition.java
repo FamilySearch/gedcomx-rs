774dda5b9c743938b73ac4e7a70d4121e7e6382d
Ryan Heaton <ryan@webcohesion.com>
2012-11-06 11:22:21 -0700

    removing resource elements from conclusion rs definition

--- gedcomx-rs/src/main/java/org/gedcomx/rs/ConclusionRSDefinition.java
+++ gedcomx-rs/src/main/java/org/gedcomx/rs/ConclusionRSDefinition.java
@@ -15,27 +15,20 @@
  */
 package org.gedcomx.rs;
 
-import org.gedcomx.conclusion.Conclusion;
-import org.gedcomx.conclusion.Fact;
-import org.gedcomx.conclusion.Gender;
-import org.gedcomx.conclusion.Name;
 import org.gedcomx.rt.CommonModels;
 import org.gedcomx.rt.rs.*;
 
 import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.core.Response;
 
 /**
- * The conclusion resource defines the interface for a specific name, gender, or fact conclusion.
+ * The conclusion resource defines the interface for managing the state of a specific conclusion (e.g. name, gender, or fact).
  *
  * @author Ryan Heaton
  */
 @ResourceDefinition (
   name = "Conclusion",
   projectId = "gedcomx-rs",
-  resourceElement = { Name.class, Gender.class, Fact.class },
   namespace = CommonModels.GEDCOMX_NAMESPACE
 )
 @ResourceLinks ({
@@ -46,31 +39,6 @@ import javax.ws.rs.core.Response;
 public interface ConclusionRSDefinition extends CommonRSParameters {
 
   public static final String REL = GEDCOMX_LINK_REL_PREFIX + "conclusion";
-
-  /**
-   * Read a conclusion.
-   *
-   * @return The conclusion.
-   */
-  @GET
-  @StatusCodes({
-    @ResponseCode ( code = 200, condition = "Upon a successful read."),
-    @ResponseCode ( code = 404, condition = "If the requested resource is not found.")
-  })
-  Response get();
-
-  /**
-   * Update a conclusion.
-   *
-   * @param conclusion The conclusion to be used for the update.
-   *
-   */
-  @PUT
-  @StatusCodes({
-    @ResponseCode ( code = 204, condition = "The update was successful."),
-    @ResponseCode ( code = 404, condition = "If the requested resource is not found.")
-  })
-  Response put(Conclusion conclusion);
 
   /**
    * Delete a conclusion.
