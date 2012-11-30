527150a156a09e4eda7f676f2c1ae86e822dd8c3
Ryan Heaton <ryan@webcohesion.com>
2012-11-30 10:13:39 -0700

    applying visitor pattern to atom feed

---
+++ gedcomx-atom/src/main/java/org/gedcomx/atom/rt/BaseAtomModelVisitor.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright 2011-2012 Intellectual Reserve, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.gedcomx.atom.rt;
+
+import org.gedcomx.Gedcomx;
+import org.gedcomx.atom.Content;
+import org.gedcomx.atom.Entry;
+import org.gedcomx.atom.Feed;
+import org.gedcomx.atom.Person;
+import org.gedcomx.rt.BasicGedcomxModelVisitor;
+
+import javax.xml.bind.annotation.XmlTransient;
+import java.util.List;
+
+/**
+ * @author Ryan Heaton
+ */
+@XmlTransient
+public class BaseAtomModelVisitor extends BasicGedcomxModelVisitor implements AtomModelVisitor {
+
+  @Override
+  public void visitFeed(Feed feed) {
+    List<Entry> entries = feed.getEntries();
+    if (entries != null) {
+      for (Entry entry : entries) {
+        visitEntry(entry);
+      }
+    }
+
+    List<Person> authors = feed.getAuthors();
+    if (authors != null) {
+      for (Person author : authors) {
+        visitAtomPerson(author);
+      }
+    }
+
+    List<Person> contributors = feed.getContributors();
+    if (contributors != null) {
+      for (Person contributor : contributors) {
+        visitAtomPerson(contributor);
+      }
+    }
+  }
+
+  @Override
+  public void visitAtomPerson(Person author) {
+    //no-op...
+  }
+
+  @Override
+  public void visitEntry(Entry entry) {
+    Content content = entry.getContent();
+    if (content != null) {
+      visitAtomContent(content);
+    }
+
+    List<Person> authors = entry.getAuthors();
+    if (authors != null) {
+      for (Person author : authors) {
+        visitAtomPerson(author);
+      }
+    }
+
+    List<Person> contributors = entry.getContributors();
+    if (contributors != null) {
+      for (Person contributor : contributors) {
+        visitAtomPerson(contributor);
+      }
+    }
+  }
+
+  @Override
+  public void visitAtomContent(Content content) {
+    Gedcomx gedcomx = content.getGedcomx();
+    if (gedcomx != null) {
+      visitGedcomx(gedcomx);
+    }
+  }
+}
